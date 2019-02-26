4366b100335063eca0b917ad3b99c93d0ade0ae2
momo <1445107381@qq.com>
2019-02-26 14:12:22 +0800

    修改样式

--- scripts/1.js
+++ scripts/1.js
@@ -1,144 +1,144 @@
 // 搜索框文字效果
-$(function(){
-    $('#inputSearch').focus(function(){
+$(function () {
+    $('#inputSearch').focus(function () {
         $(this).addClass('focus')
-        if($(this).val()===this.defaultValue){
+        if ($(this).val() === this.defaultValue) {
             $(this).val('')
         }
-    }).blur(function(){
+    }).blur(function () {
         $(this).removeClass('focus')
-        if($(this).val()===''){
+        if ($(this).val() === '') {
             $(this).val(this.defaultValue)
         }
-    }).keyup(function(e){
-        if(e.which===13){
+    }).keyup(function (e) {
+        if (e.which === 13) {
             alert('回车提交表单')
         }
     })
 })
 
 // 网页换肤
-$(function(){
-    var $li =$('#skin>li')
-    $li.click(function(){
+$(function () {
+    var $li = $('#skin>li')
+    $li.click(function () {
         switchSkin(this.id)
     })
     var cookie_skin = $.cookie('MyCssSkin')
-    if(cookie_skin){
+    if (cookie_skin) {
         switchSkin(cookie_skin)
     }
 })
-function switchSkin(skinName){
+function switchSkin(skinName) {
 
-    $('#'+skinName).addClass('selected')
-    .siblings().removeClass('selected')
-    $('#cssfile').attr('href','./styles/skin/'+skinName+'.css')
-    $.cookie('MyCssSkin', skinName,{path:'/',expires:10})
+    $('#' + skinName).addClass('selected')
+        .siblings().removeClass('selected')
+    $('#cssfile').attr('href', './styles/skin/' + skinName + '.css')
+    $.cookie('MyCssSkin', skinName, { path: '/', expires: 10 })
 }
 
 //导航效果
 
-$(function(){
-    $('#nav li').hover(function(){
+$(function () {
+    $('#nav li').hover(function () {
         $(this).find('.jnnav').show()
-        
-    },function(){
+
+    }, function () {
         $(this).find('.jnnav').hide()
     })
 })
 
 // 左侧商品分类热销效果
 
-$(function(){
+$(function () {
     $('.promoted').append('<span class="hot"></span>')
 })
 
 // 右侧上部产品广告效果
 
-$(function(){
+$(function () {
     var index = 0
-    $('#adsnav a').mouseover(function(){
-        index=$('#adsnav a').index(this)
+    $('#adsnav a').mouseover(function () {
+        index = $('#adsnav a').index(this)
         showImg(index)
     }).eq(0).mouseover()
-    var $imgroll=$('#adsnav a')
-    $imgroll.css('opacity','0.7')
-    var len =$imgroll.length
+    var $imgroll = $('#adsnav a')
+    $imgroll.css('opacity', '0.7')
+    var len = $imgroll.length
     var adTimer = null
-    $('#imageroll').hover(function(){
-        if(adTimer){clearInterval(adTimer)}
-    },function(){
-        adTimer = setInterval(function(){
+    $('#imageroll').hover(function () {
+        if (adTimer) { clearInterval(adTimer) }
+    }, function () {
+        adTimer = setInterval(function () {
             showImg(index);
             index++;
-            if(index===len){index=0}
-        },3000)
+            if (index === len) { index = 0 }
+        }, 3000)
     }).trigger('mouseleave')
 })
-function showImg(index){
+function showImg(index) {
     var $rollobj = $('#imageroll')
-    var $rolllist =$rollobj.find('div a')
-    $('#imgwrap').find('img').eq(index).stop(true,true).fadeIn().siblings().fadeOut();
-    $rolllist.removeClass('chos').css('opacity','0.7').eq(index).addClass('chos').css('opacity','1')
+    var $rolllist = $rollobj.find('div a')
+    $('#imgwrap').find('img').eq(index).stop(true, true).fadeIn().siblings().fadeOut();
+    $rolllist.removeClass('chos').css('opacity', '0.7').eq(index).addClass('chos').css('opacity', '1')
 }
 
 //右侧最新动态模块内容添加超链接提示
 
-$(function(){
+$(function () {
     var x = 10;
     var y = 20;
-    $('a.tooltip').mouseover(function(e){
+    $('a.tooltip').mouseover(function (e) {
         this.mytitle = this.title;
         this.title = '';
-        var tooltip = '<div id="tooltip">'+this.mytitle+'</div>'
+        var tooltip = '<div id="tooltip">' + this.mytitle + '</div>'
         $('body').append(tooltip);
         $('#tooltip').css({
-            'top': (e.pageY+y)+'px',
-            'left': (e.pageX+x)+'px'
+            'top': (e.pageY + y) + 'px',
+            'left': (e.pageX + x) + 'px'
         }).show('fast')
-    }).mouseout(function(){
-        this.title=this.mytitle;
+    }).mouseout(function () {
+        this.title = this.mytitle;
         $('#tooltip').remove();
-    }).mousemove(function(e){
+    }).mousemove(function (e) {
         $('#tooltip').css({
-            'top': (e.pageY+y)+'px',
-            'left': (e.pageX+x)+'px'  
+            'top': (e.pageY + y) + 'px',
+            'left': (e.pageX + x) + 'px'
         })
     })
 })
 
 // 右侧下部品牌活动横向滚动效果
 
-$(function(){
-    $('#brandtab a').click(function(){
+$(function () {
+    $('#brandtab a').click(function () {
         $(this).parent().addClass('chos').siblings().removeClass('chos');
-        var index=$('#brandtab a').index(this);
+        var index = $('#brandtab a').index(this);
         showBrandList(index);
-		return false;
-    }) .eq(0).click() 
+        return false;
+    }).eq(0).click()
 })
-function showBrandList(index){
+function showBrandList(index) {
     var $roll = $("#brandcontent");
     var rollWidth = $roll.find("li").outerWidth();
-	rollWidth = rollWidth * 4; //一个版面的宽度
-	$roll.stop(true,false).animate({ left : -rollWidth*index},1000);
+    rollWidth = rollWidth * 4; //一个版面的宽度
+    $roll.stop(true, false).animate({ left: -rollWidth * index }, 1000);
 
 }
 
 //右侧下部光标划过产品列表效果
 
-$(function(){
-    $('#brandcontent li').each(function(index){
+$(function () {
+    $('#brandcontent li').each(function (index) {
         var $img = $(this).find('img');
         var img_w = $img.width();
         var img_h = $img.height();
-        var span = '<span style="position: absolute;top:0;left: 5px;width:'+
-        img_w+'px;height:'+img_h+'px;" class="imgMask"></span>'
+        var span = '<span style="position: absolute;top:0;left: 5px;width:' +
+            img_w + 'px;height:' + img_h + 'px;" class="imgMask"></span>'
         $(span).appendTo(this)
     })
-    $('#brandcontent .imgMask').mouseover(function(){
-		$(this).addClass("imageOver");
-    }) .mouseout(function(){
-		$(this).removeClass("imageOver");
-    }) 
+    $('#brandcontent .imgMask').mouseover(function () {
+        $(this).addClass("imageOver");
+    }).mouseout(function () {
+        $(this).removeClass("imageOver");
+    })
 })
--- scripts/2.js
+++ scripts/2.js
@@ -1,75 +1,75 @@
 // 搜索框文字效果
-$(function(){
-    $('#inputSearch').focus(function(){
+$(function () {
+    $('#inputSearch').focus(function () {
         $(this).addClass('focus')
-        if($(this).val()===this.defaultValue){
+        if ($(this).val() === this.defaultValue) {
             $(this).val('')
         }
-    }).blur(function(){
+    }).blur(function () {
         $(this).removeClass('focus')
-        if($(this).val()===''){
+        if ($(this).val() === '') {
             $(this).val(this.defaultValue)
         }
-    }).keyup(function(e){
-        if(e.which===13){
+    }).keyup(function (e) {
+        if (e.which === 13) {
             alert('回车提交表单')
         }
     })
 })
 
 // 网页换肤
-$(function(){
-    var $li =$('#skin>li')
-    $li.click(function(){
+$(function () {
+    var $li = $('#skin>li')
+    $li.click(function () {
         switchSkin(this.id)
     })
     var cookie_skin = $.cookie('MyCssSkin')
-    if(cookie_skin){
+    if (cookie_skin) {
         switchSkin(cookie_skin)
     }
 })
-function switchSkin(skinName){
+function switchSkin(skinName) {
 
-    $('#'+skinName).addClass('selected')
-    .siblings().removeClass('selected')
-    $('#cssfile').attr('href','./styles/skin/'+skinName+'.css')
-    $.cookie('MyCssSkin', skinName,{path:'/',expires:10})
+    $('#' + skinName).addClass('selected')
+        .siblings().removeClass('selected')
+    $('#cssfile').attr('href', './styles/skin/' + skinName + '.css')
+    $.cookie('MyCssSkin', skinName, { path: '/', expires: 10 })
 }
 
 //导航效果
 
-$(function(){
-    $('#nav li').hover(function(){
+$(function () {
+    $('#nav li').hover(function () {
         $(this).find('.jnnav').show()
-        
-    },function(){
+
+    }, function () {
         $(this).find('.jnnav').hide()
     })
 })
 
 // 左侧商品分类热销效果
 
-$(function(){
+$(function () {
     $('.promoted').append('<span class="hot"></span>')
 })
 
 //　单击产品小图片切换大图
-$(function(){
-	$("#proitem ul.imgList li a").click(function(){
-          var imgSrc = $(this).find("img").attr("src");
-          var i = imgSrc.lastIndexOf(".");
-          var unit = imgSrc.substring(i);
-          imgSrc = imgSrc.substring(0,i);
-          var imgSrc_big = imgSrc + "_small"+ unit;
-          $(".zoomwrap a img").attr("src" , imgSrc_big);
-	});
+$(function () {
+    $("#proitem ul.imgList li a").click(function () {
+        var imgSrc = $(this).find("img").attr("src");
+        var i = imgSrc.lastIndexOf(".");
+        var unit = imgSrc.substring(i);
+        imgSrc = imgSrc.substring(0, i);
+        var imgSrc_big = imgSrc + "_small" + unit;
+        $(".zoomwrap a img").attr("src", imgSrc_big);
+    });
 });
 
 
 //产品属性介绍选项卡
 
-$(function(){
-    $('.tab_menu li').click(function(){
+$(function () {
+    $('.tab_menu li').click(function () {
         $(this).addClass('selected').siblings().removeClass('selected')
         var index = $('.tab_menu li').index(this)
         $('.tab_box div').eq(index).show().siblings().hide()
@@ -79,29 +79,29 @@ $(function(){
 
 //右侧产品颜色切换
 
-$(function(){
-    $('.color_change img').click(function(){
+$(function () {
+    $('.color_change img').click(function () {
         $(this).addClass('selected').parent().siblings().find('img').removeClass('selected')
         var imgSrc = $(this).attr('src');
-        var i =　imgSrc.lastIndexOf('.');
+        var i = imgSrc.lastIndexOf('.');
         var unit = imgSrc.substring(i);
-        imgSrc = imgSrc.substring(0,i);
-        var imgSrc_small = imgSrc+'_one_small'+unit;
-        var imgSrc_big = imgSrc+'_one_big'+unit;
-        $('#bigimage').attr('src',imgSrc_small);
-        $('.look a').attr('href',imgSrc_big);
+        imgSrc = imgSrc.substring(0, i);
+        var imgSrc_small = imgSrc + '_one_small' + unit;
+        var imgSrc_big = imgSrc + '_one_big' + unit;
+        $('#bigimage').attr('src', imgSrc_small);
+        $('.look a').attr('href', imgSrc_big);
         var alt = $(this).attr('alt');
         $('.color_change strong').text(alt);
-        var newImgSrc = imgSrc.replace('images/pro_img/','');
+        var newImgSrc = imgSrc.replace('images/pro_img/', '');
         $('.imgList li').hide();
-        $('.imgList').find('.imgList_'+newImgSrc).show()
+        $('.imgList').find('.imgList_' + newImgSrc).show()
     })
 })
 
 //　右侧产品尺寸切换
 
-$(function(){
-    $('.size li').click(function(){
+$(function () {
+    $('.size li').click(function () {
         $(this).addClass('selected').siblings().removeClass('selected')
         var alt = $(this).attr('alt')
         $('.pro_size strong').text(alt)
@@ -109,41 +109,41 @@ $(function(){
 })
 
 // 右侧产品数量和价格联动
-$(function(){
-    $('#num_sort ').change(function(){
+$(function () {
+    $('#num_sort ').change(function () {
         var index = $(this).val();
-        $('.pro_price strong').text(200*index)
+        $('.pro_price strong').text(200 * index)
     })
 })
 
 //　右侧给产品评分的效果
 
-$(function(){
-    $('ul.rating li a').click(function(){
-        var title= $(this).attr('title');
-        alert('您给本商品的评分是：'+title);
+$(function () {
+    $('ul.rating li a').click(function () {
+        var title = $(this).attr('title');
+        alert('您给本商品的评分是：' + title);
         var cl = $(this).parent().attr('class')
-        $('.pro_rating ul').removeClass().addClass('rating '+cl+'star');
+        $('.pro_rating ul').removeClass().addClass('rating ' + cl + 'star');
         return false;
     })
 })
 
 //右侧放入购物车
 
-$(function(){
+$(function () {
     var $product = $('#details');
-    $('#cart a').click(function(){
+    $('#cart a').click(function () {
         var name = $product.find('h4:first').text();
         var size = $product.find('.pro_size strong').text();
         var color = $product.find('.color_change strong').text();
         var num = $product.find('#num_sort').val();
         var price = $product.find('.pro_price strong').text();
-        var dialog = '感谢您的购买。\n您购买的\n'+
-        '产品是：'+name+';\n'+
-        '尺寸是：'+size+';\n'+
-        '颜色是：'+color+';\n'+
-        '数量是：'+num+';\n'+
-        '总价是：'+price+'元';
+        var dialog = '感谢您的购买。\n您购买的\n' +
+            '产品是：' + name + ';\n' +
+            '尺寸是：' + size + ';\n' +
+            '颜色是：' + color + ';\n' +
+            '数量是：' + num + ';\n' +
+            '总价是：' + price + '元';
         alert(dialog);
         return false;
     })
